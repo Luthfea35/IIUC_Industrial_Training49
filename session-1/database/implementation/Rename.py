92ee5392cdcb8997c701c02f4fa866c516c4f1ee
Luthfea35 <luthfeawashefa21@gmail.com>
2024-05-17 21:42:02 +0600

    Renametable

---
+++ session-1/database/implementation/Rename.py
@@ -0,0 +1,61 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+def create_db_connection():
+    """
+    Create a database connection to the MySQL database specified by the db_name.
+
+    Returns
+    -------
+    connection : mysql.connector.connection.MySQLConnection
+        The connection object to the database.
+    """
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            passwd=os.getenv("DB_PASS"),
+            database=os.getenv("DB_NAME")
+        )
+        print("MySQL Database connection successful")
+        return connection
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        return None
+def execute_query(connection, query, data=None):
+    """
+    Execute a given SQL query on the provided database connection.
+
+    Parameters
+    ----------
+    connection : mysql.connector.connection.MySQLConnection
+        The connection object to the database.
+    query : str
+        The SQL query to execute.
+    data : tuple, optional
+        The data tuple to pass to the query, for parameterized queries.
+
+    Returns
+    -------
+    None
+    """
+    cursor = connection.cursor()
+    try:
+        if data:
+            cursor.execute(query, data)
+        else:
+            cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+if __name__ == "__main__":
+    conn = create_db_connection()
+    execute_query(conn,"RENAME TABLE editors TO publishers;")
+    execute_query(conn,"RENAME TABLE authors TO reporters;")
