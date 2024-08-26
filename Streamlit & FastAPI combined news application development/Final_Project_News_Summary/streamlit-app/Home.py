8bd2b580c4e9e5f40929d45d2086c0481e3e9a6d
Luthfea35 <luthfeawashefa21@gmail.com>
2024-08-27 01:05:58 +0600

    Streamlit & FastAPI

---
+++ Streamlit & FastAPI combined news application development/Final_Project_News_Summary/streamlit-app/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="News Summarization App", layout="wide")
+
+st.header("Smart News Digest: AI-Enhanced Summaries from The Business Standard")
+st.subheader("Welcome to the future of news reading!Our advanced AI-driven system is designed to save you valuable time by delivering concise and accurate summaries of news articles from The Business Standard. No more sifting through lengthy reports—our app makes staying informed both easy and efficient, allowing you to quickly grasp the essence of any story with just a glance.")
+st.markdown("Take a glance and see how easy it is to get quick news summaries!")
